dd78093da09601a380760184259272fef6274423
codingARa <74619512+codingARa@users.noreply.github.com>
2023-12-15 22:21:00 +0100

    solution part1 working

--- Day01/Code/Program.cs
+++ Day01/Code/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Code
 {
@@ -9,17 +10,27 @@ namespace Code
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Let's solve Day XX");
+            Console.WriteLine("Let's solve Day 01");
             var input = File.ReadAllLines("input.txt")
                 .ToList();
             int answer1 = SolutionPart1(input);
             Console.WriteLine($"Solution part 1: {answer1}");
         }
 
-        public static int SolutionPart1(List<string> inputIntegers)
+        public static int SolutionPart1(List<string> input)
         {
-            //throw new NotImplementedException();
-            return 0;
+            int result = 0;
+            foreach(var line in input)
+            {
+                Regex leftRegex = new Regex(@"\d");
+                Regex rightRegex = new Regex(@"\d", RegexOptions.RightToLeft);
+                var leftMatches = leftRegex.Match(line);
+                var rightMatches = rightRegex.Match(line);
+                var leftNo = leftMatches.Value[0].ToString();
+                var rightNo = rightMatches.Value[0].ToString();
+                result += int.Parse(leftNo + rightNo);
+            }
+            return result;
         }
     }
 }
--- Day01/Test/UnitTest.cs
+++ Day01/Test/UnitTest.cs
@@ -8,9 +8,14 @@ namespace Test
         [Test]
         public void Test1()
         {
-            List<string> inputList = new() {};
+            List<string> inputList = new() {
+                "1abc2",
+                "pqr3stu8vwx",
+                "a1b2c3d4e5f",
+                "treb7uchet"
+            };
             var answer = Code.Program.SolutionPart1(inputList);
-            Assert.That(answer, Is.EqualTo(0));
+            Assert.That(answer, Is.EqualTo(142));
         }
     }
 }
