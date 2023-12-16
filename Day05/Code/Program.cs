54ef209e7034254374ffa34fb7e70d05f9e59a55
codingARa <74619512+codingARa@users.noreply.github.com>
2023-12-16 20:13:14 +0100

    setup Day05

--- Day05/Code/Program.cs
+++ Day05/Code/Program.cs
@@ -9,14 +9,14 @@ namespace Code
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Let's solve Day XX");
+            Console.WriteLine("Let's solve Day 05");
             var input = File.ReadAllLines("input.txt")
                 .ToList();
             int answer1 = SolutionPart1(input);
             Console.WriteLine($"Solution part 1: {answer1}");
         }
 
-        public static int SolutionPart1(List<string> inputIntegers)
+        public static int SolutionPart1(List<string> input)
         {
             return 0;
         }
