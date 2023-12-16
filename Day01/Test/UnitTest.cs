b81470c7978cd5abc8adddfea49a1a804af40aeb
codingARa <74619512+codingARa@users.noreply.github.com>
2023-12-16 12:27:11 +0100

    part 2 completed - first, wrong approach was saved as well

--- Day01/Test/UnitTest.cs
+++ Day01/Test/UnitTest.cs
@@ -17,5 +17,21 @@ namespace Test
             var answer = Code.Program.SolutionPart1(inputList);
             Assert.That(answer, Is.EqualTo(142));
         }
+
+        [Test]
+        public void Test2()
+        {
+            List<string> inputList = new() {
+                "two1nine",
+                "eightwothree",
+                "abcone2threexyz",
+                "xtwone3four",
+                "4nineeightseven2",
+                "zoneight234",
+                "7pqrstsixteen"
+            };
+            var answer = Code.Program.SolutionPart2(inputList);
+            Assert.That(answer, Is.EqualTo(281));
+        }
     }
 }
