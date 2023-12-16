f95fa474fb2b11492b351d43421c466188250d2e
codingARa <74619512+codingARa@users.noreply.github.com>
2023-12-16 13:14:22 +0100

    fixing BadImageFormatException in basic setup

---
+++ DayXX/Test/UnitTest.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Test
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            List<string> inputList = new() {};
+            var answer = Code.Program.SolutionPart1(inputList);
+            Assert.That(answer, Is.EqualTo(1));
+        }
+    }
+}
